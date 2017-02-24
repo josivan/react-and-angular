13681ddfd343be783bd5c7f8f5cf2564914231f3
josivan <josivanps@gmail.com>
2017-02-23 22:13:33 -0300

    First Example using ReactDOM.render

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const 
+  gulp        = require('gulp'),
+  browserify  = require('browserify'),
+  source      = require('vinyl-source-stream'),
+  gutil       = require('gulp-util'),
+  babelify    = require('babelify'),
+  clean       = require('gulp-clean');
+
+const dependencies = [
+  'react',
+  'react-dom'
+];
+
+
+gulp.task('clean', () => {
+  gulp.src(['bundle', 'dest', 'web'])
+    .pipe(clean());
+});
+
+// should be run to tranform jsx files
+gulp.task('bundle', () => {
+  let appBundler = browserify({
+    entries: [
+      'src/Login1.jsx',
+      'src/Login2.jsx'
+    ],
+    debug: true
+  });
+
+  dependencies.forEach((dep) => {
+    appBundler.external(dep);
+  });
+
+  appBundler
+    .transform(babelify)
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./web/js/'));
+});
+
+// based on http://jpsierens.com/tutorial-gulp-javascript-2015-react/
+// run at least once to bundle vendors content
+gulp.task('vendors', () => {
+  browserify({
+    require: dependencies,
+    debug: true
+  })
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('vendors.js'))
+    .pipe(gulp.dest('./web/js/'));
+});
---
+++ src/Login2.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Versão nova!</h1>,
+  document.getElementById('app2')
+);
